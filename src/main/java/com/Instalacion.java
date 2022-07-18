6781e44db4b6bfc98f575102fe2abd0fee53770c
eliaslamanna <eliaslamanna@hotmail.com>
2022-07-17 21:04:27 -0300

    fix

--- src/main/java/com/Instalacion.java
+++ src/main/java/com/Instalacion.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 public class Instalacion extends Visita {
 
-    public Instalacion(Cliente cliente, List<Usuario> tecnicos) {
-        super(cliente, tecnicos);
+    public Instalacion(Cliente cliente, List<Usuario> tecnicos, String dia, Integer horarioInicio, Integer horarioFin) {
+        super(cliente, tecnicos, dia, horarioInicio, horarioFin);
         setTiempoTrabajado(60);
         agregarArticulo(new Articulo("Cable coaxial", 4.5F));
         agregarArticulo(new Articulo("Decodificador de TV", 1F));
--- src/main/java/com/Reparacion.java
+++ src/main/java/com/Reparacion.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 public class Reparacion extends Visita {
 
-    public Reparacion(Cliente cliente, List<Usuario> tecnicos) {
-        super(cliente, tecnicos);
+    public Reparacion(Cliente cliente, List<Usuario> tecnicos, String dia, Integer horarioInicio, Integer horarioFin) {
+        super(cliente, tecnicos, dia, horarioInicio, horarioFin);
         setTiempoTrabajado(30);
     }
 }
