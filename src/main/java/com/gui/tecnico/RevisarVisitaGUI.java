6ef56955b94c9bdc49c8a56ba642a612613e8998
Facundo Messina <facuamessina@gmail.com>
2022-07-19 20:59:51 -0300

    Modify RevisarVisitaGUI

--- src/main/java/com/gui/tecnico/RevisarVisitaGUI.java
+++ src/main/java/com/gui/tecnico/RevisarVisitaGUI.java
@@ -1,11 +1,25 @@
 package com.gui.tecnico;
 
-import com.Usuario;
-
-import javax.swing.*;
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.GridLayout;
+import java.awt.Window;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+import javax.swing.border.EmptyBorder;
+
+import com.Usuario;
+import javax.swing.SwingConstants;
 
 public class RevisarVisitaGUI extends JFrame {
 
@@ -14,15 +28,23 @@ public class RevisarVisitaGUI extends JFrame {
         setTitle("Revisar Servicio");
 
         JLabel tiempoTrabajadoLabel = new JLabel(" Tiempo trabajado");
+        tiempoTrabajadoLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+        tiempoTrabajadoLabel.setBounds(0, 0, 202, 23);
         JTextField tiempoTrabajado = new JTextField();
+        tiempoTrabajado.setBounds(212, 0, 202, 23);
 
         JButton otrosCostosButton = new JButton("Agregar Otros Costos");
+        otrosCostosButton.setBounds(0, 34, 202, 23);
         JButton materialesAdicionalesButton = new JButton("Agregar Materiales Adicionales");
+        materialesAdicionalesButton.setBounds(0, 68, 202, 23);
         JButton revisarButton = new JButton("Revisar");
+        revisarButton.setBounds(0, 117, 202, 23);
         JButton cancelarButton = new JButton("Cancelar");
+        cancelarButton.setBounds(212, 117, 202, 23);
 
         JPanel revisarVisitaForm = new JPanel();
-        revisarVisitaForm.setLayout(new GridLayout(5,2,10,5));
+        revisarVisitaForm.setBounds(10, 10, 414, 151);
+        revisarVisitaForm.setLayout(null);
         revisarVisitaForm.add(tiempoTrabajadoLabel);
         revisarVisitaForm.add(tiempoTrabajado);
 
@@ -32,16 +54,16 @@ public class RevisarVisitaGUI extends JFrame {
         revisarVisitaForm.add(cancelarButton);
 
         JPanel revisarVisitaPanel = new JPanel();
-        revisarVisitaPanel.setLayout(new BorderLayout(10,10));
-        revisarVisitaPanel.add(revisarVisitaForm,BorderLayout.NORTH);
+        revisarVisitaPanel.setLayout(null);
+        revisarVisitaPanel.add(revisarVisitaForm);
         revisarVisitaPanel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
 
         cancelarButton.addActionListener(e -> cerrarVentana(e));
 
-        add(revisarVisitaPanel);
+        getContentPane().add(revisarVisitaPanel);
 
-        setSize(450,300);
-        setMinimumSize(new Dimension(450, 300));
+        setSize(450,200);
+        setMinimumSize(new Dimension(450, 200));
         setLocationRelativeTo(null);
         setVisible(true);
     }
